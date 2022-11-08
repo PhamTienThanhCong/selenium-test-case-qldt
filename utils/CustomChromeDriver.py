2ff53614432aaf0daffce270ad3895ebb7552247
congphamtienthanh <congphamtienthanh@gmail.com>
2022-11-08 14:02:20 +0700

    run test

--- utils/CustomChromeDriver.py
+++ utils/CustomChromeDriver.py
@@ -6,7 +6,7 @@ def customChrome():
     option = Options()
     option.add_argument("--enable-extentions")
 
-    driver = webdriver.Chrome(chrome_options=option, executable_path="../drivers/chromedriver.exe")
+    driver = webdriver.Chrome(chrome_options=option, executable_path="./drivers/chromedriver.exe")
     driver.implicitly_wait(3)
     driver.maximize_window()
 
